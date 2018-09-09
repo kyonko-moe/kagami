2d147593419db82433c8ca8e4b1ab1bd8497a490
muyang <muyang@bilibili.com>
2018-09-09 22:44:49 +0800

    add test doc comment

--- api/v1/http/api.go
+++ api/v1/http/api.go
@@ -1,11 +1,21 @@
+// Package http provides http api
 package http
 
 import (
 	"context"
-	"net"
 )
 
-// Register is .
+// Register a new client to server
 func Register(ctx context.Context) {
+
+}
+
+// List get all client info
+func List(ctx context.Context) {
+
+}
+
+// Connect to a spcified client , when ready it will be return
+func Connect(ctx context.Context) {
 
 }
